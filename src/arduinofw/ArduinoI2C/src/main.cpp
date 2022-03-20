f84e9d91c6f8786d5f1e1c351315d5b4a127e8fb
francescasilvano <s282021@studenti.polito.it>
2022-03-20 18:15:27 +0100

    add main.cpp for arduino firmware

--- src/arduinofw/ArduinoI2C/src/main.cpp
+++ src/arduinofw/ArduinoI2C/src/main.cpp
@@ -4,20 +4,28 @@
 void onRecv(int ln) {
 
 	while(Wire.available()) {
-		if (Wire.read() == 0x0) {
-			PORTB &= ~(1 << 5);
-			PORTB |= (Wire.read() & 0x1) << 5;
+		char readWire= Wire.read();
+		if (readWire == 0x0) {
+			PORTB &= ~(1 << 5); //xxx0xxxx
+			PORTB |= (Wire.read() & 0x1) << 5; 
 		}
 	}
 
 }
+void onReq(){
+	int readValue = analogRead(A1); //read the analog value from A1 and transform it in a value of 10 bit
+	Wire.write(readValue); // respond with message of 10 bit
 
+}
 void setup() {
-	DDRB |= (1 << 5);
-	Wire.begin(0x8);
+	//needed for the control of the I/O, 1 for output 0 for input 8 bit
+	DDRB |= (1 << 5)|(1 << 6);
+	//00110000 5 AND 6 AS OUTPUT
+	Wire.begin(0x8); //slave indirizzo 8
 	Wire.onReceive(onRecv);
+	Wire.onRequest(onReq);
 }
 
 void loop() {
-    
+	
 }
