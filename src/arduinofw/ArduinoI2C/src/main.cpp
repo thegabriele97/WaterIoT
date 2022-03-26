0fc9479cbe26b8e9386e156dd4cd6b2f26949809
Giovanni Pollo <giovannipollo98@gmail.com>
2022-03-26 20:48:44 +0100

    Fix arduino firmware

--- src/arduinofw/ArduinoI2C/src/main.cpp
+++ src/arduinofw/ArduinoI2C/src/main.cpp
@@ -3,29 +3,31 @@
 
 void onRecv(int ln) {
 
-	while(Wire.available()) {
-		char readWire= Wire.read();
-		if (readWire == 0x0) {
-			PORTB &= ~(1 << 5); //xxx0xxxx
-			PORTB |= (Wire.read() & 0x1) << 5; 
-		}
-	}
+  while(Wire.available()) {
+    char readWire= Wire.read();
+    if (readWire == 0x0) {
+      PORTB &= ~(1 << 5); //xxx0xxxx
+      PORTB |= (Wire.read() & 0x1) << 5; 
+    }
+  }
 
 }
 void onReq(){
-	int readValue = analogRead(A1); //read the analog value from A1 and transform it in a value of 10 bit
-	Wire.write(readValue); // respond with message of 10 bit
+  int readValue = analogRead(A1); //read the analog value from A1 and transform it in a value of 10 bit
+  Serial.println(readValue);
+  Wire.write((uint8_t*)&readValue, 2); // respond with message of 10 bit
 
 }
 void setup() {
-	//needed for the control of the I/O, 1 for output 0 for input 8 bit
-	DDRB |= (1 << 5)|(1 << 6);
-	//00110000 5 AND 6 AS OUTPUT
-	Wire.begin(0x8); //slave indirizzo 8
-	Wire.onReceive(onRecv);
-	Wire.onRequest(onReq);
+  //needed for the control of the I/O, 1 for output 0 for input 8 bit
+  DDRB |= (1 << 5)|(1 << 6);
+  //00110000 5 AND 6 AS OUTPUT
+  Serial.begin(9600);
+  Wire.begin(0x8); //slave indirizzo 8
+  Wire.onReceive(onRecv);
+  Wire.onRequest(onReq);
 }
 
 void loop() {
-	
+  
 }
