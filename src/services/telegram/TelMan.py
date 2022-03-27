142f76d00124bab7c0b4e2c909cf743823665be1
thabriele97 <gabriele.lagreca@gmail.com>
2022-03-27 18:37:24 +0200

    Telegram Manager: support for setCommands API. Added keryboard for switch. Updated help message

--- src/services/telegram/TelMan.py
+++ src/services/telegram/TelMan.py
@@ -1,76 +1,148 @@
 import telepot
 import time
 from telepot.loop import MessageLoop
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton ,InlineQueryResultArticle, InputTextMessageContent
+from telepot.namedtuple import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 import json
 import requests
-from encryption import *
+import logging
+from encryption import Encryption
 from common.CatalogRequest import *
+from common.WIOThread import WIOThread
+from SuperTelepotBot import SuperTelepotBot
 
 class MyBot:
    
     def __init__(self,token,logger,catreq):
-    # Local token
+        # Local token
         self.catreq = catreq
-        self.logger = logger
+        self.logger : logging.Logger = logger
         self.chat_ID = ""
         self.tokenBot=token
-    #Catalog token
-    #self.tokenBot=requests.get("http://catalogIP/telegram_token").json()["telegramToken"]
-        self.bot=telepot.Bot(self.tokenBot)
-        MessageLoop(self.bot, {'chat': self.on_chat_message}).run_as_thread() # take as input the bot to wich listen the message and the handler of the message
+        self._update_id = None
+        self._encr = Encryption()
+        #Catalog token
+        #self.tokenBot=requests.get("http://catalogIP/telegram_token").json()["telegramToken"]
+        self.bot = SuperTelepotBot(self.tokenBot)
+
+        self._commands = {
+            "commands": [
+                {"command": "help",     "description": "Show the list of all available commands"},
+                {"command": "psw",      "description": "Subscribe the user"},
+                {"command": "switch",   "description": "Turn on or off the irrigator"},
+                {"command": "getairt",  "description": "Retrieve temperature of the air"},
+                {"command": "getairu",  "description": "Retrieve humidity of the air"},
+                {"command": "getsoilu", "description": "Retrieve humidity of the soil"},
+                {"command": "config",   "description": "Sets the sampling period of the sensors"}
+            ],
+            "language_code": "en"
+        }
+
+        self.bot.setCommands(self._commands)
+
+        self._bot_th = WIOThread(target=self._handler_bot_th, name="Telegram Bot Handler")
+        self._bot_th.run()
+
+    def _handler_bot_th(self):
+
+        while not self._bot_th.is_stop_requested:
+            
+            response = None
+            try:
+                response = self.bot.getUpdates(offset=self._update_id);
+            except:
+                self.logger.exception("Bad exception occurred! Stopping Telegram Bot Handler..")
+                while not self._bot_th.is_stop_requested:
+                    self._bot_th.wait(60)
+
+                continue
+
+            for resp in response:
+                self._update_id = resp["update_id"] + 1
+
+                try:
+                    self.on_chat_message(resp["message"])
+                except Exception as e:
+                    self.logger.exception(f"Exception occurred while handling a Telegram message: {str(e)}")
+
+            self._bot_th.wait(1)
+
 
     def on_chat_message(self,msg):
+
         content_type, chat_type ,self.chat_ID = telepot.glance(msg)
         message=msg['text']
+        self.logger.debug(self.chat_ID)
+
         if message.split()[0]=="/psw":
-                if len(message.split()) == 1:
-                    self.bot.sendMessage(self.chat_ID,"No password. Please, write a password after the command.")
+            if len(message.split()) == 1:
+                self.bot.sendMessage(self.chat_ID, "No password. Please, write a password after the command.", reply_markup=ReplyKeyboardRemove())
+            else :
+                if self._encr.checkPassword(message.split()[1]):
+                    self._encr.addID(self.chat_ID) # for some reason it return the id twice from getupdate
+                    self.bot.sendMessage(self.chat_ID, "Correct password. The user is now subscribed and can access the functionality.", reply_markup=ReplyKeyboardRemove())
                 else :
-                    if checkPassword(message.split()[1]):
-                        addID(self.chat_ID) # for some reason it return the id twice from getupdate
-                        self.bot.sendMessage(self.chat_ID,"Correct password. The user is now subscribed and can access the functionality.")
-                    else :
-                        self.bot.sendMessage(self.chat_ID,"Wrond password")
-        elif not checkID(self.chat_ID) :
-            self.bot.sendMessage(self.chat_ID,"Unsubscribed user. Please insert the password using /psw <password> command")
+                    self.bot.sendMessage(self.chat_ID, "Wrond password", reply_markup=ReplyKeyboardRemove())
+        elif not self._encr.checkID(self.chat_ID) :
+            self.bot.sendMessage(self.chat_ID, "Unsubscribed user. Please insert the password using /psw <password> command", reply_markup=ReplyKeyboardRemove())
         else:
             if message.split()[0] == "/config":
                 if len(message.split()) != 3:
-                    self.bot.sendMessage(self.chat_ID,"No parameter. Please select one sensor (<temp>,<airhum>,<soilhum>) as first parameter and a value as second")
-                elif message.split()[1] not in ["temp","airhum","soilhum"]:
-                    self.bot.sendMessage(self.chat_ID,"Wrong parameter. Please select one sensor(<temp>,<airhum>,<soilhum>) as first parameter and a value as second")
+                    self.bot.sendMessage(self.chat_ID, "No parameter. Please select one sensor (<temp>,<airhum>,<soilhum>) as first parameter and a value as second", reply_markup=ReplyKeyboardRemove())
+                elif message.split()[1] not in ["temp", "airhum","soilhum"]:
+                    self.bot.sendMessage(self.chat_ID, "Wrong parameter. Please select one sensor(<temp>,<airhum>,<soilhum>) as first parameter and a value as second", reply_markup=ReplyKeyboardRemove())
                 else:
                     try: # verify if the value is an integer
                         #self.catreq.reqREST("DeviceConfig",f"DeviceConfig/configs?path=/sensors/{message.split()[1]}/sampleperiod",RequestType.PUT,{"v": int(message.split()[2])})
                         self.catreq.reqREST("DeviceConfig",f"/configs?path=/sensors/{message.split()[1]}/sampleperiod",RequestType.PUT,{"v": int(message.split()[2])})
                     except ValueError:
-                        self.bot.sendMessage(self.chat_ID,"Please insert an integer value")
+                        self.bot.sendMessage(self.chat_ID,"Please insert an integer value", reply_markup=ReplyKeyboardRemove())
 
-            elif message == "/start":
-                self.bot.sendMessage(self.chat_ID,"Hello!\n Here the commands:\n /psw <password> - Subscribe the user. \n /switch <on/off> - Turn on or off the irrigator \n/getairt - Retrive temperature of the air\n/getairu  - Retrive umidity of the air\n/getsoilu - Retrive umidity of the soil.\n /config (<temp>|<airhum>|<soilhum> ) <value> - Config the sensors: sets the period of sampling of the sensors. ")
+            elif message == "/start" or message == "/help":
+                self.bot.sendMessage(chat_id=self.chat_ID, parse_mode="MarkdownV2", reply_markup=ReplyKeyboardRemove(), text=(
+                "Hello\\!\n"
+                "Here the commands:\n"
+                "*/psw <password\\>* \\- Subscribe the user\n"
+                "*/switch <on/off\\>* \\- Turn on or off the irrigator\n"
+                "*/getairt* \\- Retrive temperature of the air\n"
+                "*/getairu*  \\- Retrive umidity of the air\n"
+                "*/getsoilu* \\- Retrive umidity of the soil\n"
+                "*/config* \\(<temp\\>\\|<airhum\\>\\|<soilhum\\>\\) <value\\> \\- Config the sensors: sets the period of sampling of the sensors\n"))
             elif message == "/getairt":
-                self.bot.sendMessage(self.chat_ID,"You will get air temperature")
-                self.bot.sendMessage(self.chat_ID,self.catreq.reqREST("RaspberryDevConn","/airtemperature"))
+                self.bot.sendMessage(self.chat_ID,"You will get air temperature", reply_markup=ReplyKeyboardRemove())
+                self.bot.sendMessage(self.chat_ID,self.catreq.reqREST("RaspberryDevConn","/airtemperature"), reply_markup=ReplyKeyboardRemove())
             elif message == "/getairu":
-                self.bot.sendMessage(self.chat_ID,"You will get air umidity")
-                self.bot.sendMessage(self.chat_ID,self.catreq.reqREST("RaspberryDevConn","/airhumidity"))
+                self.bot.sendMessage(self.chat_ID,"You will get air umidity", reply_markup=ReplyKeyboardRemove())
+                self.bot.sendMessage(self.chat_ID,self.catreq.reqREST("RaspberryDevConn","/airhumidity"), reply_markup=ReplyKeyboardRemove())
             elif message == "/getsoilu":
                 self.bot.sendMessage(self.chat_ID,"You will get soil umidity")
-                self.bot.sendMessage(self.chat_ID,self.catreq.reqREST("RaspberryDevConn","/terrainhumidity"))
+                self.bot.sendMessage(self.chat_ID,self.catreq.reqREST("RaspberryDevConn","/terrainhumidity"), reply_markup=ReplyKeyboardRemove())
             elif message.split()[0]=="/switch":
                 if len(message.split()) == 1:
-                    self.bot.sendMessage(self.chat_ID,"No parameter. Please, use '/switch on' or '/switch off'.")
+                    self.bot.sendMessage(chat_id=self.chat_ID,
+                        text="No parameter. Please, use '/switch on' or '/switch off'.", 
+                        reply_markup=ReplyKeyboardMarkup(
+                            keyboard=[[KeyboardButton(text="/switch on"), KeyboardButton(text="/switch off")]],
+                            resize_keyboard=True))
                 elif message.split()[1].lower() == "on":
-                    self.bot.sendMessage(self.chat_ID,"You started irrigation")
-                    self.catreq.reqREST("ArduinoDevConn","/switch?state='on'")
+                    r = self.catreq.reqREST("ArduinoDevConn","/switch?state=on")
+
+                    if not r.status or r.code_response != 200:
+                        self.bot.sendMessage(self.chat_ID, f"Error: code {r.code_response} - f{r.json_response}", reply_markup=ReplyKeyboardRemove())
+                    else:
+                        self.bot.sendMessage(self.chat_ID, "You started irrigation", reply_markup=ReplyKeyboardRemove())
+
                 elif message.split()[1].lower() == "off":
-                    self.bot.sendMessage(self.chat_ID,"You stopped irrigation")
-                    self.catreq.reqREST("ArduinoDevConn","/switch?state='off'")
+                    r = self.catreq.reqREST("ArduinoDevConn","/switch?state=off")
+
+                    if not r.status or r.code_response != 200:
+                        self.bot.sendMessage(self.chat_ID, f"Error: code {r.code_response} - f{r.json_response}", reply_markup=ReplyKeyboardRemove())
+                    else:
+                        self.bot.sendMessage(self.chat_ID, "You stopped irrigation", reply_markup=ReplyKeyboardRemove())
+
                 else:
-                    self.bot.sendMessage(self.chat_ID,"Wrong parameter. Please, use 'on' or 'off'.")
+                    self.bot.sendMessage(self.chat_ID, "Wrong parameter. Please, use 'on' or 'off'.", reply_markup=ReplyKeyboardRemove())
             else:
-                self.bot.sendMessage(self.chat_ID,"Wrong command. Please use one on the list")
+                self.bot.sendMessage(self.chat_ID, "Wrong command. Please type /help to know the list of available commands", reply_markup=ReplyKeyboardRemove())
         
         #self.bot.sendMessage(chat_ID,text="You sent:\n"+message)# send a message on the chat
     
--- src/services/telegram/encryption.py
+++ src/services/telegram/encryption.py
@@ -2,63 +2,59 @@
 import bcrypt 
 import json
 
-def setPassword():
-    data = {
-    'hashed' : '',
-    'ids' : []
-    }
-    password = "Polito22"
-    password = password.encode('utf-8')     # Encode password into a readable utf-8 byte code: 
-    data['hashed'] = bcrypt.hashpw(password, bcrypt.gensalt()).decode('ascii')     # Hash the ecoded password and generate a salt: 
-    with open('data.json', 'w') as json_file:
-        json.dump(data, json_file)
+from common.SettingsManager import SettingsManager
+from common.JSONManager import JSONManager
 
-def checkPassword(check):
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-    # Encode the authenticating password as well:
-    check = check.encode('utf-8') 
-    # Use conditions to compare the authenticating password with the stored one:
-    if bcrypt.checkpw(check, data['hashed'].encode('utf-8')):
-        return True
-    else:
-        return False
+class Encryption:
 
-def changePassword(old,new):
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-    old = old.encode('utf-8')
-    if bcrypt.checkpw(old,data['hashed'].encode('utf-8')):
-        new = new.encode('utf-8')
+    def __init__(self) -> None:
+        self._data = JSONManager(SettingsManager.relfile2abs("data.json"), structure_mod_allowed=True)
+
+    def setPassword(self):
+
         data = {
+            'hashed' : '',
+            'ids' : []
+        }
+
+        password = "Polito22"
+        password = password.encode('utf-8')     # Encode password into a readable utf-8 byte code: 
+        data['hashed'] = bcrypt.hashpw(password, bcrypt.gensalt()).decode('ascii')     # Hash the ecoded password and generate a salt: 
+        self._data.set('/', data)
+
+    def checkPassword(self, check):
+        data = self._data.get('/')
+        # Encode the authenticating password as well:
+        check = check.encode('utf-8') 
+        # Use conditions to compare the authenticating password with the stored one:
+        return bcrypt.checkpw(check, data['hashed'].encode('utf-8'))
+
+    def changePassword(self, old, new):
+        data = self._data.get('/')
+        old = old.encode('utf-8')
+
+        if bcrypt.checkpw(old,data['hashed'].encode('utf-8')):
+            new = new.encode('utf-8')
+            data = {
                 'hashed' : '',
                 'ids' : []
-                }
-        data['hashed'] = bcrypt.hashpw(new, bcrypt.gensalt()).decode('ascii')
-        with open('data.json', 'w') as json_file:
-            json.dump(data, json_file)
-        return True
-    else :
+            }
+            data['hashed'] = bcrypt.hashpw(new, bcrypt.gensalt()).decode('ascii')
+            self._data.set('/', data)
+            return True
+
         return False
 
-def addID(new):
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-    if new not in data["ids"] :
-        data['ids'].append(new)
-        with open('data.json', 'w') as json_file:
-            json.dump(data, json_file)
-        return True
-    else:
+    def addID(self, new):
+
+        data = self._data.get('/')
+        if new not in data["ids"] :
+            data['ids'].append(new)
+            self._data.set("/", data)
+            return True
+
         return False
 
-def checkID(check):
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-    if check not in data["ids"] :
-        return False
-    else:
-        return True
-
-
-    
+    def checkID(self, check):
+        data = self._data.get('/')
+        return check in data["ids"]
